659eb3ff0c220213154c0f475f81617120a291d7
Quinten <quinten.geeraerts@student.ucll.be>
2019-12-16 15:41:59 +0100

    Enroler Form page

--- src/main/java/ucll/project/ui/controller/Handler/EnrolHandler.java
+++ src/main/java/ucll/project/ui/controller/Handler/EnrolHandler.java
@@ -5,13 +5,18 @@ import ucll.project.domain.user.UserService;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class Index extends RequestHandler {
-    public Index(String command, UserService userService) {
+public class EnrolHandler extends RequestHandler {
+    public EnrolHandler(String command, UserService userService) {
         super(command, userService);
     }
 
     @Override
     public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
+        String vraag1 = request.getParameter("vraag1");
+        String vraag2 = request.getParameter("vraag2");
+        String vraag3 = request.getParameter("vraag3");
+
+        System.out.println(vraag1 + vraag2 +vraag3);
         return "index.jsp";
     }
 }
