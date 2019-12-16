0586b3cb372ebd7edb0fbf5d59211dfc9b2946bd
Kalimath <r0661942@ucll.be>
2019-12-16 15:35:49 +0100

    user story 2

--- src/main/java/ucll/project/domain/user/UserService.java
+++ src/main/java/ucll/project/domain/user/UserService.java
@@ -1,12 +1,16 @@
 package ucll.project.domain.user;
 
+import ucll.project.db.WaitingList;
+
 import java.util.List;
 
 public class UserService {
     private UserRepository userRepo;
+    private WaitingList waitingList;
 
     public UserService(){
         userRepo = new UserRepositoryMemory();
+        waitingList = new WaitingList();
     }
 
     public List<User> getUsers(){
