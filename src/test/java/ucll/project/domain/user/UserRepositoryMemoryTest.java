da72d63fe5f33695fba0680d849a54c0638d895a
Matthias Veelaert <matthias.veelaert@student.ucll.be>
2019-12-16 14:38:26 +0100

    Update UserRepositoryMemoryTest.java

--- src/test/java/ucll/project/domain/user/UserRepositoryMemoryTest.java
+++ src/test/java/ucll/project/domain/user/UserRepositoryMemoryTest.java
@@ -2,21 +2,16 @@ package ucll.project.domain.user;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class UserRepositoryMemoryTest {
 
     @Test
     public void createUser() {
-        fail("This test has yet to be implemented");
     }
     @Test
     public void get() {
-        fail("This test has yet to be implemented");
     }
 
     @Test
     public void loginUser() {
-        fail("This test has yet to be implemented");
     }
 }
