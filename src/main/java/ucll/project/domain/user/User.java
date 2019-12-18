b615e671c700b740cf4779fc0cd5d6d3f132b260
Andreas <andreas.geysegoms@student.ucll.be>
2019-12-18 12:01:33 +0100

    localtime voor admin bois
    Also charizard

--- src/main/java/ucll/project/domain/user/User.java
+++ src/main/java/ucll/project/domain/user/User.java
@@ -4,6 +4,8 @@ import javax.xml.bind.DatatypeConverter;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,12 +13,14 @@ public class User extends Person {
     private static int count = 0;
     private int userId;
     private boolean difficult = false;
+    private LocalTime tijdVanInschrijving;
 
     public User(){
         setUserId(++count);
         if (count >= 100) {
             count = 1;
         }
+        tijdVanInschrijving = LocalTime.now();
     }
 
     public User(String userName, String firstName, String lastName, String email, Gender gender) {
@@ -26,6 +30,7 @@ public class User extends Person {
         if (count >= 100) {
             count = 1;
         }
+        tijdVanInschrijving = LocalTime.now();
     }
 
     // Getters and setters and toString
@@ -44,5 +49,14 @@ public class User extends Person {
 
     public void setUserId(int userId) {
         this.userId = userId;
+    }
+
+    public String getTijdVanInschrijving() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm");
+        return tijdVanInschrijving.format(dtf);
+    }
+
+    public void setTijdVanInschrijving(LocalTime tijdVanInschrijving) {
+        this.tijdVanInschrijving = tijdVanInschrijving;
     }
 }
